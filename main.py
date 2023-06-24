439d3f417293d04b2bc07ec0e809b72c34c3e238
Nikita Grigorenko <57573137+NikitaGrigorenko@users.noreply.github.com>
2023-06-24 18:23:17 +0300

    Main script for running pages

---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import json
+from main_page import MainPage
+from editor_page import ExpenseTracker
+from PyQt5.QtWidgets import QApplication
+
+
+class Application:
+    """
+    Represents the main application for the expense tracker.
+
+    This class manages the switching between the main page and the editor page,
+    loads data from a JSON file, and runs the application.
+    """
+
+    def __init__(self):
+        """
+        Initializes the Application class.
+
+        It sets up the necessary objects and connects the signals and slots
+        for switching between pages.
+        """
+        self.app = QApplication(sys.argv)
+        self.editor_page = ExpenseTracker()
+        self.main_page = MainPage()
+
+        self.editor_page.return_main_button.clicked.connect(
+            self.switch_to_main_page)
+        self.main_page.editor_button.clicked.connect(
+            self.switch_to_editor_page)
+
+        self.switch_to_main_page()
+
+    def switch_to_main_page(self):
+        """
+        Switches to the main page.
+
+        Loads data, updates the main page with the loaded data, closes the
+        editor page, and shows the main page.
+        """
+        self.load_data()
+        self.main_page.update_data(self.data)
+        self.editor_page.close()
+        self.main_page.show()
+
+    def switch_to_editor_page(self):
+        """
+        Switches to the editor page.
+
+        Closes the main page and shows the editor page.
+        """
+        self.main_page.close()
+        self.editor_page.show()
+
+    def load_data(self):
+        """
+        Loads data from a JSON file.
+
+        The JSON file is expected to be named 'expenses.json' and located in
+        the same directory as the script file. The loaded data is stored in
+        the instance variable 'data'.
+        """
+        file_path = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), 'expenses.json')
+        with open(file_path) as file:
+            self.data = json.load(file)
+
+    def run(self):
+        """
+        Runs the application.
+
+        Executes the application event loop.
+        """
+        sys.exit(self.app.exec_())
+
+
+if __name__ == "__main__":
+    application = Application()
+    application.run()
