d54ec66fa92b60b26137b6a9b733f09f1188da2c
y0szx <106272450+y0szx@users.noreply.github.com>
2023-06-24 18:21:24 +0300

    updated financial_logic.py

--- financial_logic.py
+++ financial_logic.py
@@ -1,6 +1,23 @@
+import os
+import json
+
+
 class FinancialLogic:
+    """
+    Represents the financial logic for managing expenses.
+
+    This class provides methods for adding, editing, deleting, and retrieving
+    expenses. It also calculates the total expenses and saves the expenses to
+    a JSON file.
+    """
+
     def __init__(self):
-        self.expenses = []
+        """
+        Initializes the FinancialLogic class.
+
+        Sets up the initial state with an empty dictionary to store expenses.
+        """
+        self.expenses = {}
 
     def add_expense(self, amount, category, description):
         """Adds an expense to the list of expenses
@@ -12,11 +29,43 @@ class FinancialLogic:
         """
         expense = {
             'amount': amount,
-            'category': category,
             'description': description
         }
-        self.expenses.append(expense)
-        print("The expense has been added successfully.")
+        if category not in self.expenses:
+            self.expenses[category] = {
+                'items': [expense], 'sumOfAmounts': amount}
+        else:
+            self.expenses[category]['items'].append(expense)
+            self.expenses[category]['sumOfAmounts'] += amount
+        self.save_expenses_to_json()
+
+    def edit_expense(self, category, index, amount, description):
+        """Edits an existing expense
+
+        Args:
+            category (str): The category of the expense
+            index (int): The index of the expense to edit
+            amount (float): The updated amount of the expense
+            description (str): The updated description of the expense
+        """
+        if category in self.expenses and index < len(self.expenses[category]['items']):
+            old_amount = self.expenses[category]['items'][index]['amount']
+            self.expenses[category]['sumOfAmounts'] += amount - old_amount
+            self.expenses[category]['items'][index]['amount'] = amount
+            self.expenses[category]['items'][index]['description'] = description
+            self.save_expenses_to_json()
+
+    def delete_expense(self, category, index):
+        """Deletes an existing expense
+
+        Args:
+            category (str): The category of the expense
+            index (int): The index of the expense to delete
+        """
+        if category in self.expenses and index < len(self.expenses[category]['items']):
+            deleted_expense = self.expenses[category]['items'].pop(index)
+            self.expenses[category]['sumOfAmounts'] -= deleted_expense['amount']
+            self.save_expenses_to_json()
 
     def calculate_total_expenses(self):
         """Calculates the total expenses
@@ -24,7 +73,8 @@ class FinancialLogic:
         Returns:
             float: the total amount of expenses
         """
-        total_expenses = sum(expense['amount'] for expense in self.expenses)
+        total_expenses = sum(category['sumOfAmounts']
+                             for category in self.expenses.values())
         return total_expenses
 
     def get_expenses_by_category(self, category):
@@ -34,20 +84,22 @@ class FinancialLogic:
             category (str): The category of expense to retrieve
 
         Returns:
-            float: the amount of the expense of a certain category
+            list: the expenses of a certain category
         """
-        expenses_by_category = [
-            expense for expense in self.expenses if expense['category'] == category]
-        return expenses_by_category
-
-    def print_expenses(self):
-        """Prints information about all expenses
-
-        If there are no expenses, print the message "There are no costs to display."
-        """
-        if not self.expenses:
-            print("There are no costs to display.")
+        if category in self.expenses:
+            return self.expenses[category]['items']
         else:
-            for expense in self.expenses:
-                print(
-                    f"Amount: {expense['amount']}, Category: {expense['category']}, Description: {expense['description']}")
+            return []
+
+    def save_expenses_to_json(self):
+        """Saves expenses to a JSON file
+
+        Args:
+            filename (str): The name of the JSON file
+        """
+        json_data = {'money': self.expenses,
+                     'totalSum': self.calculate_total_expenses()}
+        file_path = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), 'expenses.json')
+        with open(file_path, 'w') as file:
+            json.dump(json_data, file, indent=4)
